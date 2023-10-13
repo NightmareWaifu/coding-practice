878d243760f1723e5f46fa40d28f9ede1b64cb24
NightmareWaifu <irazachary@gmail.com>
2023-10-13 17:43:15 +0800

    test gitignore for target

---
+++ rust/min_cost_climbing_stairs/target/debug/min_cost_climbing_stairs.d
@@ -0,0 +1,1 @@
+C:\Users\iraza\Documents\code\leetcode\rust\min_cost_climbing_stairs\target\debug\min_cost_climbing_stairs.exe: C:\Users\iraza\Documents\code\leetcode\rust\min_cost_climbing_stairs\src\main.rs
