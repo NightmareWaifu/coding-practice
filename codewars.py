ae47d5c6320b08b66d4d1cae16bf41525be16e0d
NightmareWaifu <irazachary@gmail.com>
2024-07-10 17:29:24 +0800

    parse bank account number

--- codewars.py
+++ codewars.py
@@ -50,6 +50,51 @@ def beggars(values, n):
     print(beggar_sum)
     return beggar_sum
 
+def parse_bank_account(bank_account):
+    #example input
+    # text":              '    _  _     _  _  _  _  _ \n'+
+    #                     '  | _| _||_||_ |_   ||_||_|\n'+
+    #                     '  ||_  _|  | _||_|  ||_| _|\n'
+
+    #maybe break down to try and get the nunbers individually first
+    #all numbers are 3 chars
+    #lets try to get the numbers individually first
+    list_of_nums = ['  ', ' _', ' _', '  ', ' _', ' _', ' _', ' _', ' _', '  |', ' _|', ' _|', '|_|', '|_ ', '|_ ', '  |', '|_|', '|_|', '  |', '|_ ', ' _|', '  |', ' _|', '|_|', '  |', '|_|', ' _|']
+    #formula is 0 +28 + 28
+    row_one = []
+    row_two = []
+    row_three = []
+
+    row_length = int(len(bank_account)/3)
+    for i in range(0,row_length-1,3):
+        #+28 
+        row_one.append(bank_account[i:(i+2)])
+        row_two.append(bank_account[i+row_length:(i+3)+row_length])
+        row_three.append(bank_account[i+(row_length*2):(i+3)+(row_length*2)])
+
+
+    print(row_one)
+    print(row_two)
+    print(row_three)
+    bank_account_number = []
+
+    new_row_length = int(row_length/3)
+    for index in range(0,new_row_length):
+        for num in range(0,9):
+            if row_one[index] == list_of_nums[num] and row_two[index] == list_of_nums[num+9] and row_three[index] == list_of_nums[num+18]:
+                bank_account_number.append(str(num+1))
+                print("Number added:",num+1)
+                break
+            elif row_one[index] == " _" and row_two[index] == "| |" and row_three[index] == "|_|":
+                bank_account_number.append(str(0))
+                print("Number added: 0")
+                break
+            else:
+                print("NOT FOUND")
+                
+    
+    print(bank_account_number)
+    return int(''.join(bank_account_number))
 #5kyu
 #4kyu
 #3kyu
@@ -60,4 +105,8 @@ def beggars(values, n):
 if __name__ == "__main__":
     #stuff
     #beggars([1,2,3,4,5],1)
-    print(size_to_number("xl"))
+    #print(size_to_number("xl"))
+    print(parse_bank_account(   ' _  _     _  _     _  _  _  _  _ \n'
+                                '|_ |_   || ||_   | _||_ |_| _||_|\n'
+                                '|_| _|  ||_||_|  ||_  _| _||_ |_|\n'))
+
